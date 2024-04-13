ea27fafbb6a46d3617bd45b31957dffaff389f62
roly1234 <rolynegro7@gmail.com>
2024-04-13 18:02:25 -0400

    labo7

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { List } from './components/List'
+import { WapperList } from './components/WrapperList'
+//import { Button } from './components/Button'
+function App() {
+  //const buttonText = "Count:";
+
+  return (
+    <>
+      <WapperList>
+      <List/>
+      </WapperList>
+    </>
+    )
+}
+
+export default App
